dbe33b31dbe1b7d04e7755bde95b2d8503dd5dc0
ChickenRed181 <chicken181.red@gmail.com>
2024-10-02 20:35:19 +0600

    Add files via upload

---
+++ data-types.js
@@ -0,0 +1,23 @@
+const age = 20
+console.log('age:', age, typeof age)
+
+const userName = 'Sam'
+console.log('name:', userName, typeof userName)
+
+const isStudent = true
+console.log('isStudent:', isStudent, typeof isStudent)
+
+const user = { id: 8, username: 'Sam' }
+console.log('user:', user, typeof user)
+
+const uniqueID = Symbol('id')
+console.log('uniqueID:', uniqueID, typeof uniqueID)
+
+const nothing = null
+console.log('nothing:', nothing, typeof nothing)
+
+const bigNumber = 1234567890123456789012345678901234567890n
+console.log('bigNumber:', bigNumber, typeof bigNumber)
+
+let Undefined
+console.log('Undefined:', Undefined, typeof undefined)
---
+++ index.js
@@ -0,0 +1,10 @@
+let num = 42
+let string = 'Hello'
+let boolean = true
+
+
+num = 'Now I am a string'
+string = 100
+boolean = null
+
+alert('numb: ' + num + '\nstring: ' + string + '\nboolean: ' + boolean)
